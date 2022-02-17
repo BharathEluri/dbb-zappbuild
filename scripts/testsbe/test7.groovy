fbdf237cdc02dc5dabccd0a08c37a65cd4d39b1e
BharathEluri <bharath.eluri@makeit-consulting.com>
2022-02-17 09:33:33 +0100

    update test7 groovy and properties

--- scripts/testsbe/test7.groovy
+++ scripts/testsbe/test7.groovy
@@ -1,19 +1,36 @@
+@groovy.transform.BaseScript com.ibm.dbb.groovy.ScriptLoader baseScript
+//compile and link db2
+//with properties file  
+//with copies in syslib
+
+import com.ibm.dbb.repository.*
+import com.ibm.dbb.dependency.*
 import com.ibm.dbb.build.*
-//compile and link batch       
+import com.ibm.dbb.build.report.*
+import com.ibm.dbb.build.html.*
+import com.ibm.dbb.build.report.records.*
+import groovy.util.*
+import groovy.transform.*
+import groovy.time.*
+import groovy.xml.*
+
+
+@Field BuildProperties props = BuildProperties.getInstance()
+
+// load build.properties
+def buildConf = "/u/adcde/dbb-zappbuild/scripts/testsbe/"
+props.load(new File("${buildConf}/properties/test7.properties"))
 
 
 println("Copying source from zFS to PDS . . .")
-def copy = new CopyToPDS().file(new File("/u/adcde/dbb-zappbuild/scripts/cobol/test7.cbl")).dataset("ADCDE.BUILD.COBOL").member("HELLO7")
+def copy = new CopyToPDS().file(new File("${props.fileName}")).dataset("${props.dsn}").member("${props.member}")
 copy.execute()
 
 println("Compiling . . .")
 
-println("Compiling .xx . .")
-
-
-def compile = new MVSExec().pgm("IGYCRCTL").parm("LIB")
-compile.dd(new DDStatement().name("SYSIN").dsn("ADCDE.BUILD.COBOL(HELLO7)").options("shr"))
-compile.dd(new DDStatement().name("SYSLIN").dsn("ADCDE.BUILD.OBJ(HELLO7)").options("shr"))
+def compile = new MVSExec().pgm("${props.compilePgm}").parm("${props.compileOptsdb2}")
+compile.dd(new DDStatement().name("SYSIN").dsn("${props.dsn}(${props.member})").options("shr"))
+compile.dd(new DDStatement().name("SYSLIN").dsn("${props.objdsn}(${props.member})").options("shr"))
 compile.dd(new DDStatement().name("SYSUT1").options("cyl space(5,5) unit(vio) new"))
 compile.dd(new DDStatement().name("SYSUT2").options("cyl space(5,5) unit(vio) new"))
 compile.dd(new DDStatement().name("SYSUT3").options("cyl space(5,5) unit(vio) new"))
@@ -32,10 +49,14 @@ compile.dd(new DDStatement().name("SYSUT15").options("cyl space(5,5) unit(vio) n
 compile.dd(new DDStatement().name("SYSUT16").options("cyl space(5,5) unit(vio) new"))
 compile.dd(new DDStatement().name("SYSUT17").options("cyl space(5,5) unit(vio) new"))
 compile.dd(new DDStatement().name("SYSMDECK").options("cyl space(5,5) unit(vio) new"))
-compile.dd(new DDStatement().name("TASKLIB").dsn("IGY620.SIGYCOMP").options("shr"))
+compile.dd(new DDStatement().name("TASKLIB").dsn("${props.compilerDsn}").options("shr"))
+compile.dd(new DDStatement().dsn("${props.db2sdsnload}").options("shr"))
+compile.dd(new DDStatement().name("DBRMLIB").dsn("${props.dbrmDsn}(${props.member})").options("shr"))
+compile.dd(new DDStatement().name("SYSLIB").dsn("${props.syslibDsn}").options("shr"))
 compile.dd(new DDStatement().name("SYSPRINT").options("cyl space(5,5) unit(vio)  new"))
-compile.copy(new CopyToHFS().ddName("SYSPRINT").file(new File("/u/adcde/out/test7.log")))
+compile.copy(new CopyToHFS().ddName("SYSPRINT").file(new File("${props.listingName}")))
 def rc = compile.execute()
+
 
 if (rc > 4)
 	println("Compile failed!  RC=$rc")
@@ -43,15 +64,15 @@ else
 	println("Compile successful!  RC=$rc")
 	
 println("Linking . . .")
-def link = new MVSExec().pgm("IEWBLINK").parm("LIST,XREF,LET,MAP,SIZE=(1400K,800K),AMODE=31,RMODE=ANY")
+def link = new MVSExec().pgm("${props.linkPgm}").parm("${props.linkOpts}")
 link.dd(new DDStatement().name("SYSUT1").options("cyl space(5,5) unit(vio) new"))
-link.dd(new DDStatement().name("SYSLIN").dsn("ADCDE.BUILD.OBJ(HELLO7)").options("shr"))
+link.dd(new DDStatement().name("SYSLIN").dsn("ADCDE.BUILD.OBJ(${props.member})").options("shr"))
 link.dd(new DDStatement().name("SYSPRINT").options("cyl space(5,5) unit(vio) new"))
-link.dd(new DDStatement().name("SYSLMOD").dsn("ADCDE.COB.LOADLIB(HELLO7)").options("shr"))
-link.dd(new DDStatement().name("TASKLIB").dsn("SYS1.LINKLIB").options("shr"))
-link.dd(new DDStatement().name("SYSLIB").dsn("CEE.SCEELKED").options("shr"))
-link.dd(new DDStatement().dsn("DSNC10.SDSNLOAD").options("shr"))
-link.copy(new CopyToHFS().ddName("SYSPRINT").file(new File("/u/adcde/out/sysprintlink7.log")))
+link.dd(new DDStatement().name("SYSLMOD").dsn("${props.syslmodDsn}(${props.member})").options("shr"))
+link.dd(new DDStatement().name("TASKLIB").dsn("${props.linkerDsn}").options("shr"))
+link.dd(new DDStatement().name("SYSLIB").dsn("${props.linksyslibDsn}").options("shr"))
+link.dd(new DDStatement().dsn("${props.db2sdsnload}").options("shr"))
+link.copy(new CopyToHFS().ddName("SYSPRINT").file(new File("${props.sysprintLinkName}")))
 def rclink = link.execute()
 
 
@@ -59,7 +80,5 @@ if (rclink > 4)
 	println("Link failed!  RC=$rclink")
 else
 	println("Link successful!  RC=$rclink")
-		
-	
 	
 	
