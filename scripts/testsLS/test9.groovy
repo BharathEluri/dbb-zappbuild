b78d71fd733051cef626f72bc6a2b0cc4e9bcaaa
Leon <Leon@192.168.2.112>
2022-02-24 14:37:42 +0100

    test 4

--- scripts/testsLS/test9.groovy
+++ scripts/testsLS/test9.groovy
@@ -1,7 +1,7 @@
 import com.ibm.dbb.build.*
 
 println("Copying source from zFS to PDS . . .")
-def copy = new CopyToPDS().file(new File("/u/mit0002/dbb-zappbuild/scripts/testsbe/cobol/test1.cbl")).dataset("ADCDE.BUILD.COBOL").member("HELLO1")
+def copy = new CopyToPDS().file(new File("/u/mit0002/dbb-zappbuild/scripts/testsLS/cobol/test1.cbl")).dataset("mit0002.BUILD.COBOL").member("HELLO1")
 copy.execute()
 
 def compile = new MVSExec().pgm("IGYCRCTL").parm("LIB")
