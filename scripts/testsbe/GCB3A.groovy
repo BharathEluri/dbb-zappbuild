fbb2fb5d4390c3a5ee3492f4c0bde1d4aa6e2e55
BharathEluri <bharath.eluri@makeit-consulting.com>
2022-03-16 09:46:03 +0100

    test08

--- scripts/testsbe/GCB3A.groovy
+++ scripts/testsbe/GCB3A.groovy
@@ -4,27 +4,20 @@ import com.ibm.dbb.build.MVSExec
 
 @Field BuildProperties props = BuildProperties.getInstance()
 
-//CLI=&C1ENVMNT(1,1)
-//TYP=&C1TY(4,4)
-//IDXTAB=&C1PRGRP(3,2)
-//TABBTC='YYYYYYYNNNNNNNNNNNNNNNNN'
-//TABDB2='NYNYYNYNNNNYNYNNYNYNNYNY'
-//TABXDL='NNNNNYYNNNNNYYNNNYYNNNYY'
-//TABCIC='NNNNNNNNNNYYYYNYYYYNYYYY'
-//TABLK2='NNNNYNNNNNNNNNNNNNNNNNNN'
-//BTC=&@@TABBTC(&@#IDXTAB,1)
-//CIC=&@@TABCIC(&@#IDXTAB,1)
-//DB2=&@@TABDB2(&@#IDXTAB,1)
-//LK2=&@@TABLK2(&@#IDXTAB,1)
-//XDL=&@@TABXDL(&@#IDXTAB,1)
-//DB2@VERS='VERSION(&C1FOOTPRT(48,16))'
-
 MVSExec sql = createSqlCommand(buildFile, logicalFile, member, logFile)
 MVSExec trn = createTrnCommand(buildFile, logicalFile, member, logFile)
 MVSExec compile = createCompileCommand(buildFile, logicalFile, member, logFile)
 MVSExec lked1 = createLked1Command(buildFile, logicalFile, member, logFile)
 MVSExec lked2 = createLked2Command(buildFile, logicalFile, member, logFile)
 MVSExec dbrmcopy = createDbrmcopyCommand(buildFile, logicalFile, member, logFile)
+
+// execute mvs commands in a mvs job
+MVSJob job = new MVSJob()
+job.start()
+if ("${props.COB3DYN}"== "N" && "${props.@DB2}" == "Y") {
+//	sql.execute()
+}
+job.stop()
 
 
 def createSqlCommand(buildFile, logicalFile, member, logFile) {
@@ -41,12 +34,6 @@ def createSqlCommand(buildFile, logicalFile, member, logFile) {
 	sql.dd(new DDStatement().name("SYSIN").dsn("&&ELEMOUT").options('shr'))
 	sql.dd(new DDStatement().name("SYSCIN").dsn("&&SYSCIN").options('cyl space(5,5) unit(vio) new').pass(true))
 }
-
-def RC = 0
-if (RC ==0 && ${props.COB3DYN}== "N" && ${props.@DB2} == "Y") {
-	sql.execute()
-}
-
 
 
 def createTrnCommand(buildFile, logicalFile, member, logFile) {
