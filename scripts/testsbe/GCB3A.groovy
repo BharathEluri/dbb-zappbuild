ebbf8dcf9c695d16dadfd9939ce2f90f1b0d3ddb
BharathEluri <bharath.eluri@makeit-consulting.com>
2022-03-17 09:16:52 +0100

    test08

--- scripts/testsbe/GCB3A.groovy
+++ scripts/testsbe/GCB3A.groovy
@@ -15,7 +15,7 @@ MVSExec dbrmcopy = createDbrmcopyCommand(buildFile, logicalFile, member, logFile
 MVSJob job = new MVSJob()
 job.start()
 if ("${props.COB3DYN}"== "N" && "${props.@DB2}" == "Y") {
-//	sql.execute()
+	//	sql.execute()
 }
 job.stop()
 
@@ -27,12 +27,27 @@ def createSqlCommand(buildFile, logicalFile, member, logFile) {
 	sql.dd(new DDStatement().dsn("${props.DB2LOAD}").options("shr"))
 	sql.dd(new DDStatement().name("SYSPRINT").dsn("&&SQLLIST").options('cyl space(5,5) unit(vio) new').pass(true))
 	sql.dd(new DDStatement().name("SYSTERM").options("DUMMY"))
-	sql.dd(new DDStatement().name("SYSUT1").options("trk space(15,15) unit(vio) new"))
-	sql.dd(new DDStatement().name("SYSUT2").options("trk space(5,5) unit(vio) new"))
+	// können bestehenden trk space options behalten werden ? denn sie geben error.  
+	//	sql.dd(new DDStatement().name("SYSUT1").options("trk space(15,15) unit(vio) new"))
+	//  sql.dd(new DDStatement().name("SYSUT2").options("trk space(5,5) unit(vio) new"))
+	//  com.ibm.dbb.build.BuildException: BGZTK0016E An error occurred running BPXWDYN command 'al
+	//  loc dd(SYSUT1) trk space(15,15) unit(vio) new'
+	//  com.ibm.dbb.build.BuildException: BGZTK0016E An error occurred running BPXWDYN command 'al
+	//  loc dd(SYSUT2) trk space(5,5) unit(vio) new'
+	sql.dd(new DDStatement().name("SYSUT1").options("cyl space(5,5) unit(vio) new"))
+	sql.dd(new DDStatement().name("SYSUT2").options("cyl space(5,5) unit(vio) new"))	
+	// werden DB2DCLG dsn  immer vorher vorhanden sein ? oder werden SQL INCLUDEs (DCLGENS) im repository vorhanden sein ?
 	sql.dd(new DDStatement().name("SYSLIB").dsn("${props.DB2DCLG}").options("shr"))
-	sql.dd(new DDStatement().name("DBRMLIB").dsn("&&DBRM"("${props.C1ELEMENT}").options("cyl space(1,1,1) unit(work) new recfm(F,B) blksize(80)").pass(true)))
+    // sql.dd(new DDStatement().name("DBRMLIB").dsn("&&DBRM"("${props.C1ELEMENT}").options("cyl space(1,1,1) unit(work) new recfm(F,B) blksize(80)").pass(true)))
+	sql.dd(new DDStatement().name("DBRMLIB").dsn("${props.dbrmDsn}(${props.member})").options("shr"))	
 	sql.dd(new DDStatement().name("SYSIN").dsn("&&ELEMOUT").options('shr'))
 	sql.dd(new DDStatement().name("SYSCIN").dsn("&&SYSCIN").options('cyl space(5,5) unit(vio) new').pass(true))
+	//im JCL ist SYSIN &&ELEMOUT, SYSCIN ist &&SYSCIN  
+	//im dbb ist SYSIN ${hlq}.COBOL($member) Bespiel SAMPAPP.BUILD.COBOL(XXXX),   SYSCIN ist  &&SYSCIN
+	
+	
+	
+	
 }
 
 
