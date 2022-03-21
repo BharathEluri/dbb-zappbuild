debe39ec6b38d2e60eba82adb9d923f9dcf85b99
BharathEluri <bharath.eluri@makeit-consulting.com>
2022-03-21 11:09:13 +0100

    update file

--- scripts/testsbe/GCB3A.groovy
+++ scripts/testsbe/GCB3A.groovy
@@ -15,7 +15,18 @@ MVSExec dbrmcopy = createDbrmcopyCommand(buildFile, logicalFile, C1ELEMENT, logF
 MVSJob job = new MVSJob()
 job.start()
 if ("${props.COB3DYN}"== "N" && "${props.@DB2}" == "Y") {
-	//	sql.execute()
+//	def sqlrc = sql.execute()
+	if (sqlrc > 4)
+		println("Pre Compile failed!  RC=$sqlrc")
+	else
+	{
+		println("Pre Compile successful!  RC=$sqlrc")
+		def copyrc= copySyscIn.execute()
+		if (copyrc > 4)
+			println("copy failed!  RC=$copyrc")
+		else
+			println("copy successful!  RC=$copyrc")
+	}
 }
 job.stop()
 
@@ -24,7 +35,7 @@ def createSqlCommand(buildFile, logicalFile, C1ELEMENT, logFile) {
 	def sql = new MVSExec().pgm("DSNHPC").parm("${props.DB2OPT}")
 	sql.dd(new DDStatement().name("TASKLIB").dsn("${props.DB2EXIT}").options("shr"))
 	sql.dd(new DDStatement().dsn("${props.DB2LOAD}").options("shr"))
-	sql.dd(new DDStatement().name("SYSPRINT").dsn("&&SQLLIST").options('cyl space(5,5) unit(vio) new').pass(true))
+	sql.dd(new DDStatement().name("SYSPRINT").dsn("&&SQLLIST").options("cyl space(5,5) unit(vio) new").pass(true))
 	sql.dd(new DDStatement().name("SYSTERM").options("DUMMY"))
 	sql.dd(new DDStatement().name("SYSUT1").options("tracks space(15,15) unit(vio) new"))
 	sql.dd(new DDStatement().name("SYSUT2").options("tracks space(5,5) unit(vio) new"))
@@ -32,10 +43,27 @@ def createSqlCommand(buildFile, logicalFile, C1ELEMENT, logFile) {
 	sql.dd(new DDStatement().name("DBRMLIB").dsn("${props.dbrmDsn}(${C1ELEMENT})").options("shr"))
 	sql.dd(new DDStatement().name("SYSIN").dsn("${props.sysinDsn}(${C1ELEMENT})").options("shr"))	
 	sql.dd(new DDStatement().name("SYSCIN").dsn("&&SYSCIN").options('cyl space(5,5) unit(vio) new').pass(true))
+
+}
+
+def createcopySyscInCommand(String buildFile, LogicalFile logicalFile, String C1ELEMENT, File logFile) {
+	def copy = new MVSExec().pgm("IEBGENER")
+	copy.dd(new DDStatement().name("SYSUT1").dsn("&&SYSCIN").options("shr"))
+	copy.dd(new DDStatement().name("SYSUT2").dsn("${props.sysinDsn}(${C1ELEMENT}").options("shr"))
+	return copy
 }
 
 
+
+
 def createTrnCommand(buildFile, logicalFile, C1ELEMENT, logFile) {
+	def trn = new MVSExec().pgm("DFHECP1$").parm("{props.CITRNOPT}")
+	trn.dd(new DDStatement().name("TASKLIB").dsn("${props.CICSLOAD}").options("shr"))
+	trn.dd(new DDStatement().name("SYSPRINT").dsn("&&TRNLIST").options("cyl space(5,5) unit(vio) new").pass(true))	
+	
+	
+	
+	
 }
 
 def createCompileCommand(buildFile, logicalFile, C1ELEMENT, logFile) {
@@ -49,3 +77,5 @@ def createLked2Command(buildFile, logicalFile, C1ELEMENT, logFile) {
 
 def createDbrmcopyCommand(buildFile, logicalFile, C1ELEMENT, logFile) {
 }
+
+
